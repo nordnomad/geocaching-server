53c9d146b817bd4332bdf20cf975a7dcfcb10ff5
Mercury <sheff4ik2010@yandex.ru>
2014-12-24 00:44:11 +0200

    port fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
 import static spark.Spark.get;
+import static spark.SparkBase.port;
 import static utils.Loader.*;
 
 public class Main {
 
     public static void main(String[] args) {
-//        port(Integer.parseInt(System.getenv("PORT")));
+        port(Integer.parseInt(System.getenv("PORT")));
         get("/v1/info/:cacheId", (req, res) -> loadInfo(req.params(":cacheId")));
         get("/v1/comments/:cacheId", (req, res) -> loadComments(req.params(":cacheId")));
         get("/v1/images/:cacheId", (req, res) -> loadImages(req.params(":cacheId")));
