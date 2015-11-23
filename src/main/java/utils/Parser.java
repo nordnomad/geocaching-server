b983b01c0a354b09f9c3cc81cebf7d0484accde1
Yevhen_Ushakov <yevhen_ushakov@epam.com>
2015-11-23 17:52:37 +0200

    feature  - checkpoint now parsed in separate array

--- src/main/java/utils/Parser.java
+++ src/main/java/utils/Parser.java
@@ -13,6 +13,9 @@ import org.jsoup.select.Elements;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+
 public class Parser {
     public static JSONArray comments(String html) {
         Document doc = Jsoup.parse(html);
@@ -39,9 +42,10 @@ public class Parser {
 
         Elements textElements = doc.select("b u");
         JSONObject jsonObject = new JSONObject();
+        JSONArray checkpoints = new JSONArray();
         try {
-            for (int i = 0; i < textElements.size(); i++) {
-                Node node = textElements.get(i).parent();
+            for (Object textElement : textElements) {
+                Node node = ((Node) textElement).parent();
                 String key = "";
                 switch (((Element) node).text()) {
                     case "Атрибуты":
@@ -72,9 +76,14 @@ public class Parser {
                 if (result.endsWith("<hr>")) result = result.substring(0, result.length() - 4).trim();
                 if (result.endsWith("<p> <br>&nbsp;<br></p><p></p>"))
                     result = result.substring(0, result.length() - 29).trim();
+                JSONArray chp = findCheckpoints(result);
+                for (int j = 0; j < chp.length(); j++) {
+                    checkpoints.put(chp.getJSONObject(j));
+                }
                 result = insertCheckpointsLink(result);
                 jsonObject.put(key, result);
             }
+            jsonObject.put("checkpoints", checkpoints);
 
             Elements elements = doc.select("p>b");
 
@@ -86,10 +95,10 @@ public class Parser {
             jsonObject.put("author", authorObject);
             int i = 2;
             switch (getTextByIndex(elements, i)) {
-            case "Тайник в зимний период НЕДОСТУПЕН":
-            case "Традиционная часть тайника в зимний период недоступна":
-                //jsonObject.put("availability", getTextByIndex(elements, i));
-                i++;
+                case "Тайник в зимний период НЕДОСТУПЕН":
+                case "Традиционная часть тайника в зимний период недоступна":
+                    //jsonObject.put("availability", getTextByIndex(elements, i));
+                    i++;
             }
             jsonObject.put("created", getTextByIndex(elements, i++));
             jsonObject.put("updated", getTextByIndex(elements, i++));
@@ -153,23 +162,57 @@ public class Parser {
             int latitudeE6;
             int longitudeE6;
             try {
-                int degrees = Integer.parseInt(pageMatcher.group(1));
-                int minutes = Integer.parseInt(pageMatcher.group(2));
-                double milliMinutes = Double.parseDouble("." + pageMatcher.group(3));
+                int degrees = parseInt(pageMatcher.group(1));
+                int minutes = parseInt(pageMatcher.group(2));
+                double milliMinutes = parseDouble("." + pageMatcher.group(3));
                 latitudeE6 = new Sexagesimal(degrees, (double) minutes + milliMinutes).toCoordinateE6();
 
-                degrees = Integer.parseInt(pageMatcher.group(4));
-                minutes = Integer.parseInt(pageMatcher.group(5));
+                degrees = parseInt(pageMatcher.group(4));
+                minutes = parseInt(pageMatcher.group(5));
                 milliMinutes = Float.parseFloat("." + pageMatcher.group(6));
                 longitudeE6 = new Sexagesimal(degrees, (double) minutes + milliMinutes).toCoordinateE6();
             } catch (Exception e) {
+                e.printStackTrace();
                 continue;
             }
-//            pageMatcher.appendReplacement(sb, String.format("<a href=\"geo:%d,%d\" style=\"color: rgb(86,144,93)\"><b>%s</b></a>", latitudeE6, longitudeE6, pageMatcher.group(0)));
-            pageMatcher.appendReplacement(sb, String.format("<a href=\"#\" style=\"color: rgb(86,144,93)\"><b>%s</b></a>", /*latitudeE6, longitudeE6,*/ pageMatcher.group(0)));
+            pageMatcher.appendReplacement(sb, String.format("<a href=\"geo:%d,%d\" style=\"color: rgb(86,144,93)\"><b>%s</b></a>", latitudeE6, longitudeE6, pageMatcher.group()));
         }
 
         pageMatcher.appendTail(sb);
         return sb.toString();
+    }
+
+    public static JSONArray findCheckpoints(String text) {
+        if (text == null) throw new IllegalArgumentException("text is null");
+
+        text = text.replace("</strong><strong>", ""); // prepare string. actually this is server side responsibility
+
+        Matcher pageMatcher = geoPattern.matcher(text);
+        JSONArray result = new JSONArray();
+        while (pageMatcher.find()) {
+            int latitudeE6;
+            int longitudeE6;
+            try {
+                int degrees = parseInt(pageMatcher.group(1));
+                int minutes = parseInt(pageMatcher.group(2));
+                double milliMinutes = parseDouble("." + pageMatcher.group(3));
+                latitudeE6 = new Sexagesimal(degrees, (double) minutes + milliMinutes).toCoordinateE6();
+
+                degrees = parseInt(pageMatcher.group(4));
+                minutes = parseInt(pageMatcher.group(5));
+                milliMinutes = Float.parseFloat("." + pageMatcher.group(6));
+                longitudeE6 = new Sexagesimal(degrees, (double) minutes + milliMinutes).toCoordinateE6();
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
+            JSONObject checkpoint = new JSONObject();
+            checkpoint.put("lat", latitudeE6);
+            checkpoint.put("lon", longitudeE6);
+            checkpoint.put("name", pageMatcher.group());
+            result.put(checkpoint);
+        }
+
+        return result;
     }
 }
--- src/main/java/utils/Sexagesimal.java
+++ src/main/java/utils/Sexagesimal.java
@@ -1,38 +1,13 @@
 package utils;
 
-//TODO: Rename, implement gettes and setters with argument validation
-//TODO: Implement unit tests
 public class Sexagesimal {
     public int degrees;
     public double minutes;
-
-    public Sexagesimal(Sexagesimal sexagesimal) {
-        this.degrees = sexagesimal.degrees;
-        this.minutes = sexagesimal.minutes;
-    }
 
     public Sexagesimal(int degrees, double minutes) {
         //if (Math.abs(degrees) > 180 || minutes >= 60) {
         this.degrees = degrees;
         this.minutes = minutes;
-    }
-
-    public Sexagesimal(double coordinate) {
-        minutes = (Math.abs(coordinate) % 1) * 60;
-        degrees = (int) coordinate;
-    }
-
-    public Sexagesimal roundTo(int i) {
-        double precision = Math.pow(10, i);
-        Sexagesimal copy = new Sexagesimal(this);
-        copy.minutes = Math.round(minutes * precision);
-        if (copy.minutes == 60 * precision) {
-            copy.minutes = 0;
-            if (copy.degrees < 0) copy.degrees--;
-            else copy.degrees++;
-        }
-        copy.minutes /= precision;
-        return copy;
     }
 
     public double toCoordinate() {
@@ -41,7 +16,6 @@ public class Sexagesimal {
         return coordinate;
     }
 
-    @Deprecated
     public int toCoordinateE6() {
         return (int) (toCoordinate() * 1E6);
     }
